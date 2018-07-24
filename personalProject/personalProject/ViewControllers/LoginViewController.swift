76f92746d71653447a0b7196d6470a1846508efd
MelissaFlores2002 <41021321+MelissaFlores2002@users.noreply.github.com>
2018-07-24 10:08:34 -0700

    Added all files

---
+++ personalProject/personalProject/ViewControllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  personalProject
+//
+//  Created by Melissa Flores on 7/23/18.
+//  Copyright © 2018 Melissa Flores. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+    
+}
